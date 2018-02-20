2ae5e70970186780c2862dd9c8939719b6ae8b50
Manuela <fmanuela499@gmail.com>
2018-02-19 20:31:12 -0500

    funcionalidad agregar mensaje

--- public/js/index.js
+++ public/js/index.js
@@ -2,4 +2,23 @@
 
 $(document).ready(function () {
   $('.modal').modal();
+
+  // funcionalidad mensaje nuevo:
+  var posts = $('#posts');
+  var titleMessage = $('.title-message-js');
+  var message = $('.message-js');
+  var sendButton = $('.button-send-js');
+  var titleCharacters = 10;
+  var messageCharacters = 120;
+  sendButton.on('click', function (event) {
+    var tempTitle = titleMessage.val();
+    var tempMessage = message.val();
+
+    if (tempTitle.length <= titleCharacters && tempMessage.length <= messageCharacters) {
+      var template = '<div class="row">\n      <div class="col s10  offset-s1 z-depth-2 border ">\n      <h1 class="center-align">' + tempTitle + '</h1>\n      <p class="center-align">' + tempMessage + '</p>\n      </div>\n    </div>';
+      posts.append(template);
+      titleMessage.val('');
+      message.val('');
+    }
+  });
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
   $('.modal').modal();
+
+  // funcionalidad mensaje nuevo:
+  const posts = $('#posts');
+  let titleMessage = $('.title-message-js');
+  let message = $('.message-js');
+  let sendButton = $('.button-send-js');
+  const titleCharacters = 10;
+  const messageCharacters = 120;
+  sendButton.on('click', function(event) {
+    let tempTitle = titleMessage.val();
+    let tempMessage = message.val();
+     
+    if (tempTitle.length <= titleCharacters && tempMessage.length <= messageCharacters) {
+      let template = `<div class="row">
+      <div class="col s10  offset-s1 z-depth-2 border ">
+      <h1 class="center-align">${tempTitle}</h1>
+      <p class="center-align">${tempMessage}</p>
+      </div>
+    </div>`;
+      posts.append(template);
+      titleMessage.val('');
+      message.val('');
+    }
+  });
 });
