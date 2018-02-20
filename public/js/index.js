e815280e533fcdd1be84d5ef4d321627ef05d155
Rocciescobar <roccisen.88@gmail.com>
2018-02-19 20:37:42 -0500

    Avance funcionalidad agregar imagen

--- public/js/index.js
+++ public/js/index.js
@@ -2,4 +2,18 @@
 
 $(document).ready(function () {
   $('.modal').modal();
+
+  var containerPosts = $('#posts');
+  // Cargar imágen
+
+  var inputTitleImg = $('#input_title_img');
+  // let inputImg = $('#inputImg');
+
+  $('#inputImg').on('change', handleFileSelect, false);
+
+  var handleFileSelect = function handleFileSelect(evt) {
+    // let selectedImg = evt.target.files;
+    // console.log(selectedImg);    
+    console.log('hola');
+  };
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-$(document).ready(function() {
+$(document).ready(() => {
   $('.modal').modal();
+
+  const containerPosts = $('#posts');
+  // Cargar imágen
+  
+  let inputTitleImg = $('#input_title_img');
+  // let inputImg = $('#inputImg');
+
+  $('#inputImg').on('change', handleFileSelect, false);
+
+  let handleFileSelect = (evt) => {
+    // let selectedImg = evt.target.files;
+    // console.log(selectedImg);    
+    console.log('hola');    
+  };
 });
